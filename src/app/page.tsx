f772dbc42582fe84bd83649fb15212bb19c288e8
joe thuku <jthuku490@gmail.com>
2024-10-20 18:49:52 +0300

    Update app name in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Crypto Exchange</h1>
+      <h1 className={styles.title}>Welcome to Element Pay Easily spend your crypto to local currency and vice versa</h1>
       <div className={styles.buttonContainer}>
         <Link href="/buy/amount" className={styles.button}>Buy Crypto</Link>
         <Link href="/sell/amount" className={styles.button}>Sell Crypto</Link>
